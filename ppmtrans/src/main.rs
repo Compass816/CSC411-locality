4ff42c0ef351034825941232e571d9ab16a149f4
Ben Keeler <71103393+benkeeler12@users.noreply.github.com>
2023-10-25 23:04:13 -0400

    Update main.rs

--- ppmtrans/src/main.rs
+++ ppmtrans/src/main.rs
@@ -46,64 +46,27 @@ fn main() {
     }
 }
 
-/*     if second_arg == "rotate180" {
-        if let Some(value) = &input3 {
-            if value == "col-major" {
-            for (x, y, &element) in try_2.iter_col_major() {
-                let rotated_x = width2 - 1 - x;
-                let rotated_y = height2 - 1 - y;
-
-                if rotated_x < try_3.width() && rotated_y < try_3.height() {
-                    let new = try_3.get_mut(rotated_x, rotated_y);
-                    if let Ok(new) = try_3.get_mut(rotated_x, rotated_y) {
-                        *new = element.clone();
-                    } else {
-                        // Handle the case where the index is out of bounds.
-                    }
-                  }
-                }
-
-            for (x, y, &element) in try_3.iter_row_major(){
-                println!("x:{}y:{}, {}", x,y,element)
-            }
-
-
-         process::exit(0);
-
-        }
-    }
-
-
-
-        for (x, y, &element) in try_2.iter_row_major() {
-            let rotated_x = width2 - 1 - x;
-            let rotated_y = height2 - 1 - y;
-            println!("{}", rotated_x);
-
-            if rotated_x < try_3.width() && rotated_y < try_3.height() {
-                let new = try_3.get_mut(rotated_x, rotated_y);
-                if let Ok(new) = try_3.get_mut(rotated_x, rotated_y) {
-                    *new = element.clone();
-                } else {
-                    // Handle the case where the index is out of bounds.
-                }
-             }
-            }
-
-        for (x, y, &element) in try_3.iter_row_major(){
-            println!("x:{}y:{}, {}", x,y,element)
-
-        }
-
-
-    }
-}
-*/
 
 fn rotate90_rmo(img: &Array2<usize>) -> Array2<usize> {
     let mut rotated_img = Array2::blank_state(img.height(), img.width(), 0);
 
     for (x, y, &element) in img.iter_row_major() {
+        let rotated_x = img.height() - y - 1;
+        let rotated_y = x;
+
+        if rotated_x < rotated_img.width() && rotated_y < rotated_img.height() {
+            let new = rotated_img.get_mut(rotated_x, rotated_y);
+            *new = element.clone();
+        }
+    }
+
+    rotated_img
+}
+
+fn rotate90_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut rotated_img = Array2::blank_state(img.height(), img.width(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
         let rotated_x = img.height() - y - 1;
         let rotated_y = x;
 
@@ -132,8 +95,25 @@ fn rotate180_rmo(img: &Array2<usize>) -> Array2<usize> {
     rotated_img
 }
 
+fn rotate180_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut rotated_img = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
+        let rotated_x = img.width() - x - 1;
+        let rotated_y = img.height() - y - 1;
+
+        if rotated_x < rotated_img.width() && rotated_y < rotated_img.height() {
+            let new = rotated_img.get_mut(rotated_x, rotated_y);
+            *new = element.clone();
+        }
+    }
+
+    rotated_img
+}
+
+
 fn rotate270_rmo(img: &Array2<usize>) -> Array2<usize> {
-    let mut rotated_img = Array2::blank_state(img.height(), img.width(), 0);
+    let mut rotated_img: Array2<usize> = Array2::blank_state(img.height(), img.width(), 0);
 
     for (x, y, &element) in img.iter_row_major() {
         let rotated_x = y;
@@ -148,8 +128,52 @@ fn rotate270_rmo(img: &Array2<usize>) -> Array2<usize> {
     rotated_img
 }
 
-fn rotate0_rmo(img: Array2<usize>) -> Array2<usize> {
-    img
+fn rotate270_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut rotated_img: Array2<usize> = Array2::blank_state(img.height(), img.width(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
+        let rotated_x = y;
+        let rotated_y = img.width() - x - 1;
+
+        if rotated_x < rotated_img.width() && rotated_y < rotated_img.height() {
+            let new = rotated_img.get_mut(rotated_x, rotated_y);
+            *new = element.clone();
+        }
+    }
+
+    rotated_img
+}
+
+
+fn rotate0_rmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut rotated_img: Array2<usize> = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_row_major() {
+        let rotated_x = x;
+        let rotated_y = y;
+
+        if rotated_x < rotated_img.width() && rotated_y < rotated_img.height() {
+            let new = rotated_img.get_mut(rotated_x, rotated_y);
+            *new = element.clone();
+        }
+    }
+
+    rotated_img
+}
+fn rotate0_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut rotated_img: Array2<usize> = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
+        let rotated_x = x;
+        let rotated_y = y;
+
+        if rotated_x < rotated_img.width() && rotated_y < rotated_img.height() {
+            let new = rotated_img.get_mut(rotated_x, rotated_y);
+            *new = element.clone();
+        }
+    }
+
+    rotated_img
 }
 
 fn flip_horizontal_rmo(img: &Array2<usize>) -> Array2<usize> {
@@ -167,10 +191,40 @@ fn flip_horizontal_rmo(img: &Array2<usize>) -> Array2<usize> {
     flipped_img
 }
 
-fn flipped_vertical(img: &Array2<usize>) -> Array2<usize> {
+fn flip_horizontal_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut flipped_img = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
+        let flipped_y = img.width() - y - 1;
+
+        if x < flipped_img.width() && flipped_y < flipped_img.height() {
+            let new = flipped_img.get_mut(x, flipped_y);
+            *new = element.clone();
+        }
+    }
+
+    flipped_img
+}
+
+fn flipped_vertical_rmo(img: &Array2<usize>) -> Array2<usize> {
     let mut flipped_img = Array2::blank_state(img.width(), img.height(), 0);
 
     for (x, y, &element) in img.iter_row_major() {
+        let flipped_x = img.height() - x - 1;
+
+        if flipped_x < flipped_img.width() && y < flipped_img.height() {
+            let new = flipped_img.get_mut(flipped_x, y);
+            *new = element.clone();
+        }
+    }
+
+    flipped_img
+}
+
+fn flipped_vertical_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut flipped_img = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
         let flipped_x = img.height() - x - 1;
 
         if flipped_x < flipped_img.width() && y < flipped_img.height() {
@@ -186,6 +240,20 @@ fn transpose_rmo(img: &Array2<usize>) -> Array2<usize> {
     let mut transposed_img: Array2<usize> = Array2::blank_state(img.width(), img.height(), 0);
 
     for (x, y, &element) in img.iter_row_major() {
+
+        if x < transposed_img.width() && y < transposed_img.height() {
+            let new = transposed_img.get_mut(y, x);
+            *new = element.clone();
+        }
+    }
+
+    transposed_img
+}
+
+fn transpose_cmo(img: &Array2<usize>) -> Array2<usize> {
+    let mut transposed_img: Array2<usize> = Array2::blank_state(img.width(), img.height(), 0);
+
+    for (x, y, &element) in img.iter_col_major() {
 
         if x < transposed_img.width() && y < transposed_img.height() {
             let new = transposed_img.get_mut(y, x);
